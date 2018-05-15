e596d0e272372daa00b7a2aa48af2e7c8fa9dae9
ziutekk <aszmidt92@gmail.com>
2018-05-15 15:51:34 +0200

    Implement all methods from diagram UML to QuestionIterator class

--- expertsystem/src/main/java/expertsystem/QuestionIterator.java
+++ expertsystem/src/main/java/expertsystem/QuestionIterator.java
@@ -1,6 +1,25 @@
 package expertsystem;
 
-public class QuestionIterator implements Iterator {
+import java.util.List;
+import java.util.Iterator; 
 
-    
+public class QuestionIterator implements Iterator<Question> {
+
+    private List<Question> list;
+    private int index;
+
+    public QuestionIterator(List<Question> list){
+        this.list = list;
+    }
+
+    public boolean hasNext(){
+        return index < list.size();
+    }
+
+    public Question next(){
+        if (this.hasNext())
+            return list.get(this.index++);
+        else
+            return null;
+    }
 }
