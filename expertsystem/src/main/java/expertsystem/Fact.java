bcfe2cea59e025b8d9a29436138202d5757a3480
SebastianS <strycharz97@gmail.com>
2018-05-15 14:29:54 +0200

    Add fields and constructor method in fact class

--- expertsystem/src/main/java/expertsystem/Fact.java
+++ expertsystem/src/main/java/expertsystem/Fact.java
@@ -6,5 +6,16 @@ import java.util.Set;
 
 public class Fact{
 
+    private String id;
+    private String description;
+    private Set<String> ids;
+    private Map<String, Boolean> values = new HashMap<>();
 
+    public Fact(String id, String description){
+        this.id = id;
+        this.description = description;
+    }
+
+
+    }
 }
