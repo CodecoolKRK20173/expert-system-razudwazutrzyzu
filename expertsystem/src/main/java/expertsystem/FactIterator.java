ec93637e994cfb4d9bd4314dcb4a8722e7bccc89
SebastianS <strycharz97@gmail.com>
2018-05-15 14:59:09 +0200

    Add FactIterator class with constructor and fields

---
+++ expertsystem/src/main/java/expertsystem/FactIterator.java
@@ -0,0 +1,17 @@
+package expertsystem;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class FactIterator implements Iterator<Fact>{
+    private int index;
+    private ArrayList<Fact> facts;
+
+    public FactIterator(ArrayList<Fact> facts){
+        this.index = 0;
+        this.facts = facts;
+    }
+
+
+
+}
