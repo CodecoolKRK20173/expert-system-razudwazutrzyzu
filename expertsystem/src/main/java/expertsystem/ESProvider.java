5120b084ad19437c5cc9287ff6f29f34d970720d
ziutekk <aszmidt92@gmail.com>
2018-05-16 13:04:55 +0200

    Add collectAnswer method

--- expertsystem/src/main/java/expertsystem/ESProvider.java
+++ expertsystem/src/main/java/expertsystem/ESProvider.java
@@ -1,14 +1,33 @@
 package expertsystem;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class ESProvider {
 
     FactRepository factRepository;
+    private Scanner reader = new Scanner(System.in);
+    private String input;
+    private Map<String, Boolean> matches;
 
     public ESProvider(FactParser factParser, RuleParser ruleParser){
-        //this.factRepository.getFactRepository();
+        this.factRepository.getFactRepository();
     }
 
     private void collectAnswers(){
+        this.matches = new HashMap<>();
+
+        while(questions.hasNext()){
+            System.out.println(next().getQuestion());
+            input = reader.nextLine();
+            
+            if(evaluateAnswerByInput(input)){
+                matches.put(questions.next().getId(), odpowiadającaMuValue);
+                break;
+            }
+            
+        }
     }
 
     private boolean getAnswerByQuestion(String questionID){
@@ -17,7 +36,5 @@ public class ESProvider {
 
     private String evaluate(){
         return null;
-    }
-
-    
+    }   
 }
