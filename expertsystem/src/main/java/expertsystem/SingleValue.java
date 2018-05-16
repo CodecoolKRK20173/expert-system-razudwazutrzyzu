f29873ddffeb7430edb080bf33560d3dae458994
SebastianS <strycharz97@gmail.com>
2018-05-16 13:10:48 +0200

    add methods and fields

--- expertsystem/src/main/java/expertsystem/SingleValue.java
+++ expertsystem/src/main/java/expertsystem/SingleValue.java
@@ -1,9 +1,21 @@
 package expertsystem;
 
+import java.util.List;
 
-public class SingleValue extends Value {
+class SingleValue extends Value {
+    private boolean selectionType;
 
     public SingleValue(String param, boolean selectionType){
-
+        this.selectionType = selectionType;
     }
+
+    public  List<String> getInputPattern(){
+         
+    }    
+
+    public  boolean getSelectionType(){
+        return this.selectionType;
+    }
+
+
 }
