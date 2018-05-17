659109db981445ff7d52beb79441132b705e4caa
Riczard <riczardkiller@gmail.com>
2018-05-17 11:38:30 +0200

    Implement RuleParser

--- expertsystem/src/main/java/expertsystem/RuleParser.java
+++ expertsystem/src/main/java/expertsystem/RuleParser.java
@@ -1,5 +1,12 @@
 package expertsystem;
 
+import java.util.List;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
 public class RuleParser extends XMLParser{
 
     RuleRepository ruleRepository;
@@ -28,8 +35,6 @@ public class RuleParser extends XMLParser{
                 String questionID = eElement.getAttribute("id");
                 String questionName = eElement.getElementsByTagName("Question").item(0).getTextContent();
 
-                
-                
                 ruleRepository.addQuestion(question);
 
                 System.out.println("Rule id : " + eElement.getAttribute("id")); //for test
@@ -38,14 +43,36 @@ public class RuleParser extends XMLParser{
                 NodeList selection = ((Element) nAnswer).getElementsByTagName("Selection");
                 for (int j = 0; j < selection.getLength(); j++) {
 
-                    Value value = new Value();
-                    
-                    Answer answer = new Answer(value);
-                    System.out.println(((Element) selection.item(j)).getAttribute("value")); //for test
+                    Answer answer = new Answer();
+
+                    Boolean state = ((Element) selection.item(j)).getAttribute("value"); //for test
+
+                    if(xElement.getElementsByTagName("SingleValue").getLength() > 0){
+                    String valueName = ((Element) xElement.getElementsByTagName("SingleValue").item(0)).getAttribute("value");
+                    Value value = new SingleValue(valueName ,state);
+                    answer.addValue(value);
+
+                    }else{
+
+                    String valueName = ((Element) xElement.getElementsByTagName("MultipleValue").item(0)).getAttribute("value");
+
+                    Value value = new MultipleValue(getValueList(valueName),state);
+                    answer.addValue(value);
+                    }
                 }
                 Question question = new Question(questionID, questionName, answer);
             }
         }
+    }
+
+    private List<String> getValueList(String valueName){
+        List<String> valueList = new List<String>();
+
+        String[] splitedValue = valueName.split(",");
+        for(int i = 0; i < splitedValue.length; i++){
+            valueList.add(splitedValue[i]);
+        }
+        return valueList;
     }
     
 }
